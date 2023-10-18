4263590fd97495f78c5e03c08b618aedb8bd3dd0
LenfIcefrost <david77988@gmail.com>
2023-10-18 21:22:58 +0800

    feat: player anim, slash anim

--- Assets/Script/Entity/Player/Normal/Player.cs
+++ Assets/Script/Entity/Player/Normal/Player.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : BaseEntity {
+    protected enum PlayerDirection {
+        Up,
+        Down,
+        Left,
+        Right
+    }
     [SerializeField]
     protected float runSpeed;
+    protected float runMulti;
     protected float runDirection;
+    protected PlayerDirection playerDirection;
     List<KeyCode> actionCodes;
     public GameObject slash;
     
@@ -36,22 +45,61 @@ public class Player : BaseEntity {
             actionCodes[other] = keyCode;
         }
     }
+    private Vector3 DirectionToForward(PlayerDirection playerDirection) {
+        switch (playerDirection) {
+            case PlayerDirection.Up:
+                return Vector3.up;
+            case PlayerDirection.Down:
+                return Vector3.down;
+            case PlayerDirection.Left:
+                return Vector3.left;
+            case PlayerDirection.Right:
+                return Vector3.right;
+            default:
+                return Vector3.zero;
+        }
+    }
+    private float DiectionToRotate(PlayerDirection playerDirection) {
+        switch (playerDirection) {
+            case PlayerDirection.Up:
+                return 180f;
+            case PlayerDirection.Down:
+                return 0f;
+            case PlayerDirection.Left:
+                return 270f;
+            case PlayerDirection.Right:
+                return 90f;
+            default:
+                return 0f;
+        }
+    }
     protected override void Start() {
     }
     protected override void Update() {
+        PlayerDirection lastPlayerState = this.playerDirection;
         base.direction.x = Input.GetKey(this.actionCodes[0]) ? (Input.GetKey(this.actionCodes[1]) ? 0 : 1) : (Input.GetKey(this.actionCodes[1]) ? -1 : 0);
         base.direction.y = Input.GetKey(this.actionCodes[2]) ? (Input.GetKey(this.actionCodes[3]) ? 0 : 1) : (Input.GetKey(this.actionCodes[3]) ? -1 : 0);
-        this.runSpeed = Input.GetKey(KeyCode.C) ? 5f : 1f;
-        base.animator.SetInteger("xspeed", Convert.ToInt16(base.direction.x * this.runSpeed));
-        base.animator.SetInteger("yspeed", Convert.ToInt16(base.direction.y * this.runSpeed));
-        if(Input.GetKeyDown(this.actionCodes[0]) || Input.GetKeyDown(this.actionCodes[1]) || Input.GetKeyDown(this.actionCodes[2]) || Input.GetKeyDown(this.actionCodes[3])){
+        this.playerDirection = Input.GetKey(this.actionCodes[0]) ? (Input.GetKey(this.actionCodes[1]) ? this.playerDirection : PlayerDirection.Right) : (Input.GetKey(this.actionCodes[1]) ? PlayerDirection.Left : this.playerDirection);
+        this.playerDirection = Input.GetKey(this.actionCodes[2]) ? (Input.GetKey(this.actionCodes[3]) ? this.playerDirection : PlayerDirection.Up) : (Input.GetKey(this.actionCodes[3]) ? PlayerDirection.Down : this.playerDirection);
+        this.runMulti = Input.GetKey(KeyCode.C) ? this.runSpeed : 1f;
+        base.animator.SetInteger("xspeed", Convert.ToInt16(math.sign(base.direction.x) * math.ceil(this.runMulti)));
+        base.animator.SetInteger("yspeed", Convert.ToInt16(math.sign(base.direction.y) * math.ceil(this.runMulti)));
+        bool actionKeyDown = this.actionCodes.Any(x => Input.GetKeyDown(x));
+        bool actionKey = this.actionCodes.Any(x=>Input.GetKey(x));
+        bool actionKeyUp = this.actionCodes.Any(x=>Input.GetKeyUp(x));
+        // if(actionKeyDown || ( actionKey && actionKeyUp)){
+        //     base.animator.SetTrigger("walk");
+        // }
+        if(actionKeyDown || (lastPlayerState != this.playerDirection)){
             base.animator.SetTrigger("walk");
         }
         if(Input.GetKeyDown(KeyCode.K)){
             this.ShuffleInput();
         }
         if(Input.GetKeyDown(KeyCode.T)){
-            // this.slash.transform.position = this.gameObject.transform.position
+            this.slash.transform.position = this.gameObject.transform.position + this.DirectionToForward(this.playerDirection) * 0.2f;
+            this.slash.transform.rotation = quaternion.identity;
+            this.slash.transform.Rotate(Vector3.forward, DiectionToRotate(this.playerDirection));
             this.slash?.SetActive(true);
             base.animator.SetTrigger("attack");
         }
@@ -64,6 +112,6 @@ public class Player : BaseEntity {
     }
     protected override void FixedUpdate() {
         base.FixedUpdate();
-        this.rigidBody2D.velocity = this.rigidBody2D.velocity * runSpeed;
+        this.rigidBody2D.velocity = this.rigidBody2D.velocity * this.runMulti;
     }
 }
