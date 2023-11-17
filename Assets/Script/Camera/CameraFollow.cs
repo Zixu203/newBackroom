789829c5e57044c4873b73b98b12ad3028ae943c
LenfIcefrost <david77988@gmail.com>
2023-11-18 01:36:21 +0800

    feat: camera vignette with player health point

--- Assets/Script/Camera/CameraFollow.cs
+++ Assets/Script/Camera/CameraFollow.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.Tilemaps;
 
 public class CameraFollow : MonoBehaviour {
+    public PostProcessVolume postProcessVolume;
+    public Vignette vignette;
+    public void Awake() {
+        postProcessVolume.profile.TryGetSettings<Vignette>(out vignette);
+    }
     void Start() {
         
     }
     private void LateUpdate() {
+        vignette.intensity.value = 1.0f - (float)(GameController.getInstance.targetPlayer.Attribute.hp / GameController.getInstance.targetPlayer.Attribute.maxHp * 0.5f);
         this.transform.position = new Vector3(
             GameController.getInstance.targetPlayer.transform.position.x,
             GameController.getInstance.targetPlayer.transform.position.y,
