dbb4dacd4c4c8a86875bd2cb0c9f17902aa5bb36
LenfIcefrost <david77988@gmail.com>
2023-11-19 01:49:32 +0800

    feat: curebag healing

--- Assets/Script/Entity/Build/Curebag/Curebag.cs
+++ Assets/Script/Entity/Build/Curebag/Curebag.cs
@@ -7,9 +7,9 @@ public class Curebag : BaseBuild
     // Start is called before the first frame update
     public GameObject curebag;
     //public BoxCollider2D KeyCollider2D;
-    public override void BeenInteract()
-    {
+    public override void BeenInteract() {
         this.gameObject.SetActive(false);
+        GameController.getInstance.targetPlayer.Attribute.Heal(20);
         
         //GameController.getInstance.targetPlayer.have_door_key = true;
         //KeyCollider2D.isTrigger = false;
