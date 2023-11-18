9fccc00cdf29d3a05c39832ec436d1cb5e4512a1
Zixu <yolly1977@gmail.com>
2023-11-18 22:51:39 +0800

    Revert "Conflict :("
    This reverts commit 3d8976f14c5668c8901886c5685fb6f264b97d39.

--- Assets/Script/GameController/GameController.cs
+++ Assets/Script/GameController/GameController.cs
@@ -4,33 +4,31 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
     public Player targetPlayer;
     public static GameController instance;
-    public static GameController getInstance
-    {
-        get
-        {
+    public static GameController getInstance {
+        get {
             if (GameController.instance == null)
                 return GameController.instance = new GameController();
             return GameController.instance;
         }
-
+        
     }
     public EnemySpawner enemySpawner;
     public GamingPoolSystem gamingPool;
+    [SerializeField]
+    public List<BaseEntity> npcs;
     public DialogueSystem dialogueSystem;
     public InGameUIController inGameUIController;
-    void Awake()
-    {
+    void Awake() {
         GameController.instance = this;
     }
-    void Start()
-    {
+    void Start() {
         SaveLoader.Load();
         this.inGameUIController = new InGameUIController();
         this.inGameUIController.init();
+        this.dialogueSystem = new DialogueSystem();
         this.dialogueSystem.init();
         DontDestroyOnLoad(this.gameObject);
         SceneManager.sceneLoaded += OnSceneLoaded;
@@ -39,30 +37,24 @@ public class GameController : MonoBehaviour
         this.enemySpawner.Start();
     }
 
-    void Update()
-    {
+    void Update() {
         this.enemySpawner.Update();
     }
-    public void changeWorld()
-    {
+    public void changeWorld() {
         //in backroom scene
-        if (SceneManager.GetActiveScene().name == "BackRoomScenes")
-        {
+        if(SceneManager.GetActiveScene().buildIndex == 2) {
             SceneManager.LoadScene("SimilarWorldScenes");
         }
         //in similarWorld scene
-        if (SceneManager.GetActiveScene().name == "SimilarWorldScenes")
-        {
+        if(SceneManager.GetActiveScene().buildIndex == 3) {
             SceneManager.LoadScene("BackRoomScenes");
         }
     }
-    private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
+    private static void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         GameController.getInstance.targetPlayer = GameObject.Find("player").GetComponent<Player>();
     }
 
-    public void LoadInGame()
-    {
+    public void LoadInGame() {
 
     }
 
