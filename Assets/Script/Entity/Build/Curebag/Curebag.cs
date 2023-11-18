af9538009479e74fbd001a10a6ae8ca37c3aeee1
JRChang <JRChang@JRPC>
2023-11-18 21:43:03 +0800

    feat: curebag

---
+++ Assets/Script/Entity/Build/Curebag/Curebag.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curebag : BaseBuild
+{
+    // Start is called before the first frame update
+    public GameObject curebag;
+    //public BoxCollider2D KeyCollider2D;
+    public override void BeenInteract()
+    {
+        this.gameObject.SetActive(false);
+        
+        //GameController.getInstance.targetPlayer.have_door_key = true;
+        //KeyCollider2D.isTrigger = false;
+    }
+}
