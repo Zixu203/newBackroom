68f9e10f47e523ac8af309c4e1f740daae287853
LenfIcefrost <david77988@gmail.com>
2023-11-20 14:33:42 +0800

    feat: extends interactor count

--- Assets/Script/Entity/Entity/InteractableEntity.cs
+++ Assets/Script/Entity/Entity/InteractableEntity.cs
@@ -1,12 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractableEntity : BaseEntity {
-    [SerializeField] private GameObject interactHint;
+    // [SerializeField] private GameObject interactHint;
+    [SerializeField] private List<GameObject> interactHints;
     public void ShowInteractor() {
-        if(this.interactHint) this.interactHint.SetActive(true);
+        // if(this.interactHint) this.interactHint.SetActive(true);
+        if(this.interactHints.Count > 0) this.interactHints.ForEach(x => x.SetActive(true));
     }
     public void HideInteractor() {
-        if(this.interactHint) this.interactHint.SetActive(false);
+        // if(this.interactHint) this.interactHint.SetActive(false);
+        if(this.interactHints.Count > 0) this.interactHints.ForEach(x => x.SetActive(false));
     }
     public virtual void BeenInteract() {
         
