96133c503e8ebd20888f9d31fac9f805db06398f
LenfIcefrost <david77988@gmail.com>
2023-11-22 02:31:57 +0800

    feat: door real sound

--- Assets/Script/Entity/Build/Door/Door.cs
+++ Assets/Script/Entity/Build/Door/Door.cs
@@ -7,6 +7,9 @@ public class Door : BaseBuild {
     public GameObject door;
     public bool is_open = false;
     public BoxCollider2D BoxCollider2D;
+    public AudioSource audioSource;
+    public AudioClip doorOpenSound;
+    public AudioClip doorCloseSound;
     public override void BeenInteract() {
         if(is_open && !Input.GetKey(KeyCode.G)) {
             GameController.getInstance.changeWorld();
@@ -15,6 +18,12 @@ public class Door : BaseBuild {
         GamingPoolGameObject bubble = GameController.getInstance.gamingPool.GetGameObject("SoundBubble", this.gameObject.transform.position + Vector3.down * 2.5f, quaternion.identity);
         bubble.GetComponent<SoundBubble>().Init(this, SoundBubble.SoundBubbleType.Wood, 10);
         is_open = !is_open;
+        if(is_open) {
+            this.audioSource.clip = doorOpenSound;
+        }else{
+            this.audioSource.clip = doorCloseSound;
+        }
+        this.audioSource.Play();
         door.SetActive(is_open);
         BoxCollider2D.isTrigger = is_open;
     }
--- Assets/Script/Entity/Build/LookedDoor/LockedDoor.cs
+++ Assets/Script/Entity/Build/LookedDoor/LockedDoor.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LockedDoor : Door {
+    public AudioClip doorLockedSound;
     public override void BeenInteract() {
         if (GameController.getInstance.targetPlayer.have_door_key==false){
+            this.audioSource.clip = doorLockedSound;
+            this.audioSource.Play();
             return;
         }
         if (base.is_open == false){
